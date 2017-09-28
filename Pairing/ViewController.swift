8e7e1c1efc36e1b7c385c271c017d2736ded249f
Yaqing Wang <yaqwang@thoughtworks.com>
2017-09-28 16:30:04 +0800

    Initial Commit

---
+++ Pairing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pairing
+//
+//  Created by Yaqing Wang on 28/09/2017.
+//  Copyright © 2017 Yaqing Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
